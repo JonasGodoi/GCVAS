079108e18c04d605264efb022fd19401612765d3
Kalbiak <66517199+Kalbiak@users.noreply.github.com>
2025-08-27 18:53:08 -0300

    Update api.js

--- figmareact/src/api/api.js
+++ figmareact/src/api/api.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: process.env.REACT_APP_API_URL, // <- variável de ambiente do FRONT
+  
 });
 
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("authToken");
     if (token) {
-      config.headers["Authorization"] = `${token}`;
+      config.headers["Authorization"] = token;
     }
-    console.log(config);
     return config;
   },
   (error) => Promise.reject(error)
